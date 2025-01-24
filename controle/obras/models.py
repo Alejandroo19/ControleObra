aeca311afe6df64800392c1949e4527a9d8d0869
Alejandroo19 <alealejandrolidio@gmail.com>
2025-01-24 17:58:54 -0300

    3  commit

--- controle/obras/models.py
+++ controle/obras/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Obra(models.Model):
+    nome = models.CharField(max_length=255)
+    valor = models.DecimalField(max_digits=10, decimal_places=2)
+    status = models.CharField(
+        max_length=20,
+        choices=[
+            ('andamento', 'Andamento'),
+            ('finalizada', 'Finalizada'),
+        ],
+        default='andamento'
+    )
+
+    def __str__(self):
+        return self.nome
--- controle/obras/views.py
+++ controle/obras/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Obra
 
-# Create your views here.
+def listar_obras(request):
+    obras = Obra.objects.all()
+    return render(request, 'obra/listar_obras.html', {'obras': obras})
+
+def criar_obra(request):
+    if request.method == 'POST':
+        nome = request.POST.get('nome')
+        valor = request.POST.get('valor')
+        Obra.objects.create(nome=nome, valor=valor)
+        return redirect('listar_obras')
+    return render(request, 'criar_obra.html')
