28b0104327eacaaf2e0d04808490136565f03b2c
Alejandroo19 <alealejandrolidio@gmail.com>
2025-01-26 17:41:48 -0300

    5  commit

--- controle/obras/models.py
+++ controle/obras/models.py
@@ -15,3 +15,21 @@ class Obra(models.Model):
 
     def __str__(self):
         return self.nome
+    
+    def valor_total_adicionado(self):
+        total = sum(valor.valor for valor in self.valores.all())
+        return total
+
+    def atualizar_status(self):
+        if self.valor_total_adicionado() >= self.valor:
+            self.status = 'finalizada'
+            self.save()
+
+
+class ValorObra(models.Model):
+    obra = models.ForeignKey(Obra, related_name='valores', on_delete=models.CASCADE)
+    valor = models.DecimalField(max_digits=10, decimal_places=2)
+    descricao = models.TextField()
+
+    def __str__(self):
+        return f"{self.valor} - {self.descricao[:50]}"
