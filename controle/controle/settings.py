22a32433285aa6cfbea169a3ff48150a0f200c03
Alejandroo19 <alealejandrolidio@gmail.com>
2025-01-27 13:28:46 -0300

    12 commit

--- controle/controle/settings.py
+++ controle/controle/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-9hm%^9+$(hibkml1kl09qpe&ds3)(n(w)_)-e6b8__v(ps9!fr
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
